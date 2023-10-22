4aefc5098d4a9667943918c62215471f9efe9e45
Victor Campos <campos.viictor@gmail.com>
2023-10-22 00:35:52 -0300

    feat: add a structure and model for the landmark data

---
+++ Landmarks/Models/Landmark.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct Landmark: Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    struct Coordinates: Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
